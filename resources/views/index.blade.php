a6c867259b6f8731826283c3fd4e6ffe050bbe98
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-03 10:24:50 +0900

    [Update]ビューを変えてみる

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>MYBBS</title>
+    </head>
+    <body>
+        <h1>MYBBS</h1>
+        <ul>
+            <li>こんにちは</li>
+            <li>こんばんは</li>
+            <li>お休み</li>
+        </ul>
+    </body>
+
+</html>
+
