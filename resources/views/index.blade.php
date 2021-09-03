c93937d2a78feabbc95ab11821f37cfdb1daa5a3
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-03 14:24:42 +0900

    [Update]変更に強いリンクに変える

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,11 @@
             <h1>MYBBS</h1>
             <ul>
                 @forelse ($posts as $i => $post)
-                  <li><a href="/posts/{{ $i }}">{{ $post }}</a></li>
+                    <li>
+                        <a href="{{route('posts.show', $i)}}">
+                            {{ $post }}
+                        </a>
+                    </li>
                 @empty
                   <li>No posts yet</li>
                 @endempty
