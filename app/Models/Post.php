a91be23ce4eba3946974eb0f3f8f74b96ed2ee5d
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-04 16:41:39 +0900

    コメントアウト追記

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//Post ->posts
 class Post extends Model
 {
     use HasFactory;
