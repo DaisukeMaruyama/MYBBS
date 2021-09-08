e6d918aec9494a6f82d92db50a571b9c73728890
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-08 09:07:36 +0900

    [Update]posts.createルートの設定

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('/', [PostController::class, 'index'])
 //  ->name('posts.show');
 Route::get('/posts/{post}', [PostController::class, 'show'])
   ->name('posts.show');
+
+Route::get('/posts/create', [PostController::class, 'create'])
+  ->name('posts.create');
