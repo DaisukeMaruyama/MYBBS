887c49462b8148279873ed0b49d51944eeb9d924
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-08 09:41:14 +0900

    [Update]storeアクションを呼ぶ

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,7 +6,7 @@
         &laquo; <a href="{{ route('posts.index') }}">Back</a>
     </div>
     <h1>Add New Post</h1>
-    <form method="post" action="">
+    <form method="post" action="{{route('posts.store')}}">
         @csrf
         <div class="form-group">
             <label>
