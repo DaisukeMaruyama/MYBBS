647b6cd6c1e554e413a889acf5616dc8a2319e65
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-08 09:57:12 +0900

    [Update]エラーになった時に値を残す処理

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,7 +11,7 @@
         <div class="form-group">
             <label>
                 Title
-                <input type="text" name="title">
+                <input type="text" name="title" value="{{old('title')}}">
             </label>
             @error('title')
                 <div class="error">{{$message}}</div>
@@ -20,7 +20,7 @@
         <div class="form-group">
             <label>
                 Body
-                <textarea name="body"></textarea>
+                <textarea name="body">{{old('body')}}</textarea>
             </label>
             @error('body')
                 <div class="error">{{$message}}</div>
