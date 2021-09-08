d11fadbd1ece63ae07ddf048a64d8eeb2fe03214
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-09-08 09:33:19 +0900

    [Update]フォームの見た目を整える

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,15 +8,20 @@
     <h1>Add New Post</h1>
     <form method="post" action="">
         @csrf
-
-        <label>
-            Title
-            <input type="text" name="title">
-        </label>
-        <label>
-            Body
-            <textarea name="body"></textarea>
-        </label>
-        <button>Add</button>
+        <div class="form-group">
+            <label>
+                Title
+                <input type="text" name="title">
+            </label>
+        </div>
+        <div class="form-group">
+            <label>
+                Body
+                <textarea name="body"></textarea>
+            </label>
+        </div>
+        <div class="form-button">
+            <button>Add</button>
+        </div>
     </form>
 </x-layout>
